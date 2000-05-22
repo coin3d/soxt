8d7c6356641afc371ee498f30cf26a5f595ff954
Lars J. Aas <larsa@sim.no>
2000-05-22 15:06:48 +0000

    Stub class.

---
+++ src/Inventor/Xt/viewers/SoXtCollisionViewer.cpp
@@ -0,0 +1,166 @@
+/**************************************************************************
+ *
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+ **************************************************************************/
+
+static const char rcsid[] =
+  "$Id$";
+
+#include <assert.h>
+
+#include <Inventor/Xt/SoXtBasic.h>
+#include <Inventor/Xt/viewers/SoXtCollisionViewer.h>
+
+// *************************************************************************
+
+/*!
+*/
+
+SoXtCollisionViewer::SoXtCollisionViewer(
+  SoXtViewer * const _viewer )
+: viewer( _viewer )
+{
+  assert( viewer != NULL );
+  this->numsteps = 3;
+  this->distance = 1.0f;
+  this->collisiondetection = TRUE;
+  this->zbufferoptimize = FALSE;
+} // SoXtCollisionViewer()
+
+/*!
+*/
+
+SoXtCollisionViewer::~SoXtCollisionViewer(
+  void )
+{
+} // ~SoXtCollisionViewer()
+
+// *************************************************************************
+
+/*!
+*/
+
+SoXtViewer *
+SoXtCollisionViewer::getViewer(
+  void ) const
+{
+  return this->viewer;
+} // getViewer()
+
+/*!
+*/
+
+void
+SoXtCollisionViewer::setDistance(
+  const float distance )
+{
+  this->distance = distance;
+} // setDistance()
+
+/*!
+*/
+
+float
+SoXtCollisionViewer::getDistance(
+  void ) const
+{
+  return this->distance;
+} // getDistance()
+
+/*!
+*/
+
+void
+SoXtCollisionViewer::setNumSteps(
+  const int steps )
+{
+  this->numsteps = steps;
+} // setNumSteps()
+
+/*!
+*/
+
+int
+SoXtCollisionViewer::getNumSteps(
+  void ) const
+{
+  return this->numsteps;
+} // getNumSteps()
+
+/*!
+*/
+
+void
+SoXtCollisionViewer::setCollisionDetection(
+  const SbBool enable )
+{
+  this->collisiondetection = enable;
+} // setCollisionDetection()
+
+/*!
+*/
+
+SbBool
+SoXtCollisionViewer::getCollisionDetection(
+  void ) const
+{
+  return this->collisiondetection;
+} // getCollisionDetection()
+
+/*!
+*/
+
+void
+SoXtCollisionViewer::setZBufferOptimization(
+  const SbBool enable )
+{
+  this->zbufferoptimize = enable;
+} // setZBufferOptimization()
+
+/*!
+*/
+
+SbBool
+SoXtCollisionViewer::getZBufferOptimization(
+  void ) const
+{
+  return this->zbufferoptimize;
+} // getZBufferOptimisation()
+
+// *************************************************************************
+
+/*!
+*/
+
+void
+SoXtCollisionViewer::addCallback(
+  SoXtCollisionViewerCB * func,
+  void * user )
+{
+} // addCallback()
+
+/*!
+*/
+
+void
+SoXtCollisionViewer::removeCallback(
+  SoXtCollisionViewerCB * func,
+  void * user )
+{
+} // removeCallback()
+
+// *************************************************************************
---
+++ src/Inventor/Xt/viewers/SoXtCollisionViewer.h
@@ -0,0 +1,71 @@
+/**************************************************************************
+ *
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+ **************************************************************************/
+
+//  $Id$
+
+#ifndef SOXT_COLLISIONVIEWER_H
+#define SOXT_COLLISIONVIEWER_H
+
+#include <Inventor/SbBasic.h>
+
+class SoXtViewer;
+class SoXtCollisionViewer;
+
+// *************************************************************************
+
+typedef void SoXtCollisionViewerCB(
+  void * userdata, SoXtCollisionViewer * viewer );
+
+class SoXtCollisionViewer {
+public:
+  SoXtCollisionViewer( SoXtViewer * const viewer );
+  ~SoXtCollisionViewer(void);
+
+  SoXtViewer * getViewer(void) const;
+
+  void setDistance( const float distance );
+  float getDistance(void) const;
+
+  void setNumSteps( const int steps );
+  int getNumSteps(void) const;
+
+  void setCollisionDetection( const SbBool value = TRUE );
+  SbBool getCollisionDetection(void) const;
+
+  void setZBufferOptimization( const SbBool value = TRUE );
+  SbBool getZBufferOptimization(void) const;
+  SbBool getZBufferOptimistation(void) const // eh?
+    { return this->getZBufferOptimization(); }
+
+  void addCallback( SoXtCollisionViewerCB * func, void * user = NULL );
+  void removeCallback( SoXtCollisionViewerCB * func, void * user = NULL );
+
+private:
+  SoXtViewer * const viewer;
+
+  int numsteps;
+  float distance;
+  SbBool collisiondetection;
+  SbBool zbufferoptimize;
+
+}; // class SoXtCollisionViewer
+
+// *************************************************************************
+
+#endif // ! SOXT_COLLISIONVIEWER_H
