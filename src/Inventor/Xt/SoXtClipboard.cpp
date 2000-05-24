291f35b82a2177c29e9d8244d60bf8a04827188a
Lars J. Aas <larsa@sim.no>
2000-05-24 16:32:45 +0000

    New class SoXtClipboard (stub implementation).

---
+++ src/Inventor/Xt/SoXtClipboard.cpp
@@ -0,0 +1,175 @@
+/**************************************************************************
+ *
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+ **************************************************************************/
+
+static const char rcsid[] =
+  "$Id$";
+
+#include <Inventor/Xt/SoXtBasic.h>
+#include <Inventor/Xt/SoXtClipboard.h>
+
+// *************************************************************************
+
+SoXtClipboard::SoXtClipboard(
+  Widget widget,
+  Atom selection )
+{
+} // SoXtClipboard()
+
+SoXtClipboard::~SoXtClipboard(
+  void )
+{
+} // ~SoXtClipboard()
+
+// *************************************************************************
+
+void
+SoXtClipboard::copy(
+  SoNode * node,
+  Time when )
+{
+  SOXT_STUB();
+} // copy()
+
+void
+SoXtClipboard::copy(
+  SoPath * path,
+  Time when )
+{
+  SOXT_STUB();
+} // copy()
+
+void
+SoXtClipboard::copy(
+  SoPathList * paths,
+  Time when )
+{
+  SOXT_STUB();
+} // copy()
+
+void
+SoXtClipboard::copy(
+  Atom type,
+  void * data,
+  uint32_t size,
+  Time when )
+{
+  SOXT_STUB();
+} // copy()
+
+void
+SoXtClipboard::copy( // protected
+  SoByteStream * byteStream,
+  Time when )
+{
+  SOXT_STUB();
+} // copy()
+
+void
+SoXtClipboard::paste(
+  Time when,
+  SoXtClipboardPasteCB * pastedone,
+  void * userdata )
+{
+  SOXT_STUB();
+} // paste()
+
+void
+SoXtClipboard::addPasteInterest(
+  Atom datatype,
+  SoXtClipboardImportCB * importfunc,
+  void * userdata )
+{
+  SOXT_STUB();
+} // addPasteInterest()
+
+SbBool
+SoXtClipboard::convertData( // static
+  Widget widget,
+  void * srcdata,
+  uint32_t size,
+  Atom desiredType,
+  char ** retdata,
+  uint32_t * retsize )
+{
+  SOXT_STUB();
+  return FALSE;
+} // convertData()
+
+void
+SoXtClipboard::setEmptyListOK(
+  SbBool enable )
+{
+  SOXT_STUB();
+} // setEmptyListOK()
+
+SbBool
+SoXtClipboard::isEmptyListOK(
+  void ) const
+{
+  SOXT_STUB();
+  return FALSE;
+} // isEmptyListOK()
+
+void
+SoXtClipboard::getExportTargets( // protected
+  char ** value,
+  uint32_t * length )
+{
+  SOXT_STUB();
+} // getExportTargets()
+
+Atom
+SoXtClipboard::chooseFromImportTargets( // protected
+  Atom * supportedTargets,
+  int length )
+{
+  SOXT_STUB();
+  return _XA_CLIPBOARD_;
+} // chooseFromImportTargets()
+
+SbBool
+SoXtClipboard::writeToFile( // static, protected
+  SbString & tmpfile,
+  void * srcdata,
+  uint32_t size )
+{
+  SOXT_STUB();
+  return FALSE;
+} // writeToFile()
+
+void
+SoXtClipboard::readFile( // static, protected
+  SoPathList * & paths,
+  const char * filename )
+{
+  SOXT_STUB();
+} // readFile()
+
+SoPathList *
+SoXtClipboard::readData( // static, protected
+  Widget w,
+  Atom target,
+  void * data,
+  uint32_t size )
+{
+  SOXT_STUB();
+  return (SoPathList *) NULL;
+} // readData()
+
+// *************************************************************************
---
+++ src/Inventor/Xt/SoXtClipboard.h
@@ -0,0 +1,97 @@
+/**************************************************************************
+ *
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+ **************************************************************************/
+
+//  $Id$
+
+#ifndef SOXT_CLIPBOARD_H
+#define SOXT_CLIPBOARD_H
+
+#include <inttypes.h>
+#include <X11/Xatom.h>
+#include <X11/Intrinsic.h>
+#include <Inventor/SbBasic.h>
+
+class SbDict;
+class SoByteStream;
+class SoNode;
+class SoPath;
+class SoPathList;
+class SoSelection;
+class SoXtImportInterestList;
+class SbString;
+class SbPList;
+
+// *************************************************************************
+
+typedef void SoXtClipboardPasteCB(
+               void * userdata, SoPathList * pathlist );
+typedef void SoXtClipboardImportCB(
+               void * userdata, Atom type, void * data, uint32_t size );
+
+#define _XA_CLIPBOARD_ ((Atom) 0)
+
+class SoXtClipboard {
+public:
+  SoXtClipboard( Widget widget, Atom selection = _XA_CLIPBOARD_ );
+  ~SoXtClipboard(void);
+
+  void copy( SoNode * node, Time when );
+  void copy( SoPath * path, Time when );
+  void copy( SoPathList * paths, Time when );
+  void copy( Atom type, void * data, uint32_t size, Time when );
+
+  void paste( Time when, SoXtClipboardPasteCB * pastedone,
+    void * userdata = NULL );
+
+  void addPasteInterest( Atom datatype, SoXtClipboardImportCB * importfunc,
+    void * userdata = NULL );
+
+  static SbBool convertData( Widget widget, void * srcdata, uint32_t size,
+    Atom desiredType, char ** retdata, uint32_t * retsize );
+
+  void setEmptyListOK( SbBool enable );
+  SbBool isEmptyListOK(void) const;
+
+protected:
+  Widget widget;
+  Atom clipboardAtom;
+  Time eventTime;
+
+  SoXtClipboardPasteCB * callbackFunc;
+  void * userData;
+
+  static SbDict * selOwnerList;
+
+  void copy( SoByteStream * byteStream, Time when );
+
+  SoXtImportInterestList * pasteInterest;
+  SbPList * copyInterest;
+
+  void getExportTargets( char ** value, uint32_t * length );
+  Atom chooseFromImportTargets( Atom * supportedTargets, int length );
+  static SbBool writeToFile( SbString & tmpfile, void * srcdata, uint32_t size );
+  static void readFile( SoPathList * & paths, const char * filename );
+
+  static SoPathList * readData( Widget w, Atom target, void * data, uint32_t size );
+
+}; // class SoXtClipboard
+
+// *************************************************************************
+
+#endif // ! SOXT_CLIPBOARD_H
