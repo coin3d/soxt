e62a755f6f7a36a0c0259d01f292c7d4117fb72e
Lars J. Aas <larsa@sim.no>
2000-05-30 13:58:11 +0000

    New variables for the rest of the components.

--- libconfigvars.sh
+++ libconfigvars.sh
@@ -1,5 +1,8 @@
-#! /bin/sh
+# libconfigvars.sh
 # no to be executed - this script must be sourced
+
+############################################################################
+# Source Substitutions:
 
 GUI="Xt"
 lGUI="xt"
@@ -8,14 +11,27 @@ WIDGET="Widget"
 EVENT="XAnyEvent *"
 COMPONENTHEADER=""
 
+export GUI lGUI uGUI WIDGET EVENT COMPONENTHEADER
+
+############################################################################
+# Build Conditionals:
+
 HAVE_RENDERAREA=true
 HAVE_EXAMINERVIEWER=true
 HAVE_PLANEVIEWER=true
 HAVE_WALKVIEWER=true
 HAVE_FLYVIEWER=true
+HAVE_LIGHTSLIDERSET=false
+HAVE_MATERIALSLIDERSET=false
+HAVE_TRANSFORMSLIDERSET=false
+HAVE_DIRECTIONALLIGHTEDITOR=false
+HAVE_MATERIALEDITOR=false
+HAVE_MATERIALLIST=false
+HAVE_PRINTDIALOG=false
 
-export GUI lGUI uGUI
-export WIDGET EVENT COMPONENTHEADER
 export HAVE_RENDERAREA HAVE_EXAMINERVIEWER
 export HAVE_PLANEVIEWER HAVE_WALKVIEWER HAVE_FLYVIEWER
+export HAVE_LIGHTSLIDERSET HAVE_MATERIALSLIDERSET HAVE_TRANSFORMSLIDERSET
+export HAVE_DIRECTIONALLIGHTEDITOR HAVE_MATERIALEDITOR
+export HAVE_MATERIALLIST HAVE_PRINTDIALOG
 
