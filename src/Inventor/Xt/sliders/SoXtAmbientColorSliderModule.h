128183f33e18450fdb76932eda40ac9f42237682
Lars J. Aas <larsa@sim.no>
2000-06-02 15:21:06 +0000

    New slider classes.  Everything but SoXtSliderSetModule will probably be moved to be autogenerated from the common code module in a while.

---
+++ src/Inventor/Xt/sliders/SoXtAmbientColorSliderModule.h
@@ -0,0 +1,47 @@
+/**************************************************************************
+ *
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+ **************************************************************************/
+
+//  $Id$
+
+#ifndef SOXT_AMBIENTCOLORSLIDERMODULE_H
+#define SOXT_AMBIENTCOLORSLIDERMODULE_H
+
+#include <Inventor/Xt/sliders/SoXtSliderSetModule.h>
+
+// *************************************************************************
+
+class SoXtAmbientColorSliderModule : public SoXtSliderSetModule {
+  typedef SoXtSliderSetModule inherited;
+
+public:
+  SoXtAmbientColorSliderModule(
+    Widget parent = NULL,
+    const char * const name = NULL,
+    const SbBool embed = TRUE,
+    SoNode * const node = NULL );
+  ~SoXtAmbientColorSliderModule(void);
+
+private:
+  virtual void valueChanged( float value, int slider );
+
+}; // class SoXtAmbientColorSliderModule
+
+// *************************************************************************
+
+#endif // ! SOXT_AMBIENTCOLORSLIDERMODULE_H
---
+++ src/Inventor/Xt/sliders/SoXtLightIntensitySliderModule.cpp
@@ -0,0 +1,73 @@
+/**************************************************************************
+ *
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+ **************************************************************************/
+
+#if SOXT_DEBUG
+static const char rcsid[] =
+  "$Id$";
+#endif // SOXT_DEBUG
+
+#include <Inventor/Xt/sliders/SoXtLightIntensitySliderModule.h>
+
+/*!
+  \class SoXtLightIntensitySliderModule Inventor/Xt/sliders/SoXtLightIntensitySliderModule.h
+  \brief The SoXtLightIntensitySliderModule class is yet to be documented.
+
+  This class is internal to SoXt (consider it private) and may change
+  interface without notice.
+*/
+
+// *************************************************************************
+
+/*!
+*/
+
+SoXtLightIntensitySliderModule::SoXtLightIntensitySliderModule(
+  const Widget parent, 
+  const char * const name, 
+  const SbBool embed, 
+  SoNode * const node )
+: inherited( parent, name, embed, node, FALSE )
+{
+} // SoXtLightIntensitySliderModule()
+
+/*!
+*/
+
+SoXtLightIntensitySliderModule::~SoXtLightIntensitySliderModule(
+  void )
+{
+} // ~SoXtLightIntensitySliderModule()
+
+// *************************************************************************
+
+/*!
+*/
+
+void
+SoXtLightIntensitySliderModule::valueChanged( // virtual, private
+  float value,
+  int slider )
+{
+} // valueChanged()
+
+// *************************************************************************
+
+#if SOXT_DEBUG
+static const char * getSoXtLightIntensitySliderModuleRCSId(void) { return rcsid; }
+#endif // SOXT_DEBUG
