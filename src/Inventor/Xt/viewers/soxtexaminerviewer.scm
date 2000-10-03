1d708e6dd3426246be509915b372a89cf4541e31
Morten Eriksen <mortene@sim.no>
2000-10-03 10:28:05 +0000

    Ivy Scheme code for testing the SoXtExaminerViewer.

---
+++ src/Inventor/Xt/viewers/soxtexaminerviewer.scm
@@ -0,0 +1,37 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Copyright (C) 1998-2000 by Systems in Motion. All rights reserved.
+;;; Written by mortene@sim.no, 2000-09-11.
+
+;;; Eval following region
+
+(define viewer (new-soxtexaminerviewer))
+(-> viewer 'setscenegraph (new-socone))
+(-> viewer 'show)
+
+;;; End initial eval-region
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; To informally test SoXtExaminerViewer, play around ;;;;;;;;;;;;;;;;;;;;;
+
+(-> viewer 'setviewing 0)
+(-> viewer 'setviewing 1)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Demonstrates Bugzilla #221 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(let ((event-root (new-sogroup))
+      (cube (new-socube))
+      (event-node (new-SoEventCallback)))
+  ; Register callback
+  (-> event-node 'addEventCallback
+      (SoKeyboardEvent::getClassTypeId)
+      (get-scheme-event-callback-cb)
+      (void-cast (callback-info (lambda (user-data event-callback)
+                                  (format #t "A key was pressed~%")))))
+  ; Set up scenegraph
+  (-> event-root 'addchild cube)
+  (-> event-root 'addchild event-node)
+  ; Attach to viewer
+  (-> viewer 'setscenegraph event-root)
+  (-> viewer 'viewall))
