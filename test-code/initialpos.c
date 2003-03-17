11404880040e1a9108bde3181d1f2795b2ef2b3c
Lars J. Aas <larsa@sim.no>
2003-03-17 17:12:24 +0000

    clean up a bit - add code that _should have_ taken care of the problem

--- test-code/initialpos.c
+++ test-code/initialpos.c
@@ -1,3 +1,12 @@
+/*
+ * This minimal program is for trying to fix the problem of Motif
+ * placing new windows at position 0,0.  Some WMs are aware of that
+ * bug and ignores it, but some WMs aren't, so it is best to fix it
+ * on the application side.
+ *
+ * gcc -I/usr/X11R6/include -L/usr/X11R6/lib -o initialpos initialpos.c -lXm -lXt -lX11
+ */
+
 #include <stdio.h>
 
 #include <X11/Intrinsic.h>
@@ -8,22 +17,51 @@
 int
 main(int argc, char ** argv)
 {
-  Display * display = XOpenDisplay(NULL);
+  Display * display;
   XtAppContext context;
-  Widget toplevel = XtVaOpenApplication(&context, "SoXt", NULL, 0, &argc, argv,
-                                        NULL, topLevelShellWidgetClass, NULL);
-  XtVaSetValues(toplevel,
-      XtNtitle, "TestWin",
-      XtNwidth, 400,
-      XtNheight, 300,
-      NULL); 
-  XSizeHints * hints = XAllocSizeHints();
-  hints->width = 400;
-  hints->height = 300;
-  hints->flags = PSize;
-  XSetWMNormalHints(display, XtWindow(toplevel), hints);
+  XSizeHints * hints;
+  long supplied;
+  Widget toplevel, w;
+  Window toplevelwin;
+  
+  setbuf(stdout, NULL);
+  setbuf(stderr, NULL);
+
+  display = XOpenDisplay(NULL);
+  toplevel = XtVaOpenApplication(&context, "SoXt", NULL, 0, &argc, argv,
+                                 NULL, topLevelShellWidgetClass, NULL);
+  XtVaSetValues(toplevel, XtNtitle, "TestWin", XtNwidth, 400, XtNheight, 300, NULL); 
   XtRealizeWidget(toplevel);
+
+  /*
+   ** One of the hints in the WM_NORMAL_HINTS protocol, PPlacement, gets set by
+   ** the X toolkit (probably part of the Core or Shell widget) when a shell
+   ** widget is realized to the value stored in the XmNx and XmNy resources of the
+   ** Core widget.  While callers can set these values, there is no "unset" value
+   ** for these resources.  On systems which are more Motif aware, a PPosition
+   ** hint of 0,0, which is the default for XmNx and XmNy, is interpreted as,
+   ** "place this as if no hints were specified".  Unfortunately the fvwm family
+   ** of window managers, which are now some of the most popular, interpret this
+   ** as "place this window at (0,0)".  This routine intervenes between the
+   ** realizing and the mapping of the window to remove the inappropriate
+   ** PPlacement hint.
+   */
+
+  w = toplevel;
+  // while ( !XtIsWMShell(w) ) w = XtParent(w);
+  toplevelwin = XtWindow(w);
+  fprintf(stderr, "toplevelwin: %ld\n", toplevelwin);
+
+  hints = XAllocSizeHints();
+
+  /* FIXME: why does the next line crash with BadWindow when linking with Motif? */
+  if ( XGetWMNormalHints(display, toplevelwin, hints, &supplied) ) {
+    fprintf(stderr, "removing position hint\n");
+    /* Get rid of the incorrect WMNormal PPosition hint */
+    hints->flags &= ~PPosition;
+    XSetWMNormalHints(display, toplevelwin, hints);
+  }
+  XFree(hints);
   XtAppMainLoop(context);
 }
-
 
