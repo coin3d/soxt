85813c9c490b3344abfeea4fa8acd564cb1bcd94
Lars J. Aas <larsa@sim.no>
2003-03-24 11:16:33 +0000

    insert coloreditor in 3D scene

--- test-code/coloreditor.cpp
+++ test-code/coloreditor.cpp
@@ -1,6 +1,8 @@
 #include <Inventor/nodes/SoSeparator.h>
 #include <Inventor/nodes/SoMaterial.h>
 #include <Inventor/nodes/SoCube.h>
+#include <Inventor/nodes/SoTranslation.h>
+#include <Inventor/nodes/SoRotationXYZ.h>
 
 #include <Inventor/Xt/SoXt.h>
 #include <Inventor/Xt/SoXtColorEditor.h>
@@ -22,12 +24,27 @@ main(int argc, char ** argv)
 {
   Widget w = SoXt::init(argc, argv, "SoXtColorEditor");
   SoXtExaminerViewer * viewer = new SoXtExaminerViewer(w);
-  viewer->setSceneGraph(makescene());
+  SoSeparator * root;
+  viewer->setSceneGraph(root = makescene());
   SoXtColorEditor * editor = new SoXtColorEditor;
   editor->attach(&(material->diffuseColor));
-  // editor->setCurrentSliders(SoXtColorEditor::RGB_HSV);
   viewer->show();
   editor->show();
+
+  SoSeparator * editorscene = new SoSeparator;
+  SoTranslation * trans = new SoTranslation;
+  trans->translation.setValue(SbVec3f(2.0f, 0.0f, 0.0f));
+  SoRotationXYZ * rot = new SoRotationXYZ;
+  SoMaterial * mat = new SoMaterial;
+  mat->diffuseColor.setValue(0.8, 0.8, 0.8);
+  rot->axis = SoRotationXYZ::Y;
+  rot->angle = 0.5;
+  editorscene->addChild(trans);
+  editorscene->addChild(rot);
+  editorscene->addChild(mat);
+  editorscene->addChild(editor->getSceneGraph());
+  root->insertChild(editorscene, 0);
+
   SoXt::show(w);
   SoXt::mainLoop();
   return 0;
