0224c305c6bfa68fd04cb329f81a307af5d04331
Volker Enderlein <volkerenderlein@hotmail.com>
2021-03-21 15:08:50 +0100

    Fix typo

--- src/Inventor/Xt/widgets/SoXtSlider.cpp
+++ src/Inventor/Xt/widgets/SoXtSlider.cpp
@@ -110,7 +110,7 @@ SoXtSlider::~SoXtSlider(
   void)
 {
   delete [] this->title;
-  delete this->callbacks
+  delete this->callbacks;
 } // ~SoXtSlider()
 
 // *************************************************************************
